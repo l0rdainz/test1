e59ede54523ae1f283fa12c1364916b6a2018891
l0rdainz <ng.liling17@gmail.com>
2022-05-11 17:45:04 +0800

    added delete button

--- eggsbee-app/src/components/Myrecipe.js
+++ eggsbee-app/src/components/Myrecipe.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import {useAuth0} from "@auth0/auth0-react";
 
-function Myrecipe() {
+
+function Myrecipe({mine}) {
+    const {user,isAuthenticated} = useAuth0();
+  
+    
   return (
-    <div>Myrecipe</div>
+    <div>{isAuthenticated ? (
+        <span>Test</span>
+    ): <span>Not logged in. Please log in to view your recipe.</span>}</div>
   )
+
 }
 
 export default Myrecipe
--- eggsbee-app/src/pages/eggsbee.js
+++ eggsbee-app/src/pages/eggsbee.js
@@ -1,9 +1,11 @@
 import React, {useState,useEffect} from 'react'
 import BannerImage from "../assets/explore.webp";
 import '../styles/eggsbee.css';
+import Myrecipe from '../components/Myrecipe';
 import axios from 'axios';
 import { Card,Button,Row,Container,Col,Modal} from "react-bootstrap";
 import {useAuth0} from "@auth0/auth0-react";
+import {FaEdit, FaTrash} from 'react-icons/fa';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
@@ -16,22 +18,32 @@ function Eggsbee() {
   const[Premium,SetPremium] = useState(false)
   const[Imgurl,Setimgurl]=useState("")
   const[submitted,SetSubmitted] = useState(false)
-  // const[Mine,SetMine] = useState(false)
-  const[Addrecipe,SetAddrecipe] = useState(false)
+  const[mine,SetMine] = useState([])
+  const[Myrecipe,SetMyrecipe] = useState([])
   // const[Top,SetTop] = useState(false)
   const [cardbutton,SetCardbutton]= useState(true)
-  const [selectedArray,setSelectedArray]=useState([])
+  const [mycardbutton,SetMycardbutton] = useState(false)
+  const [selectedArray,setSelectedArray]=useState([""])
   const {user} = useAuth0();
   const handleExplore =() =>{
-    SetAddrecipe(false)
+    document.getElementById('explore').style.display = 'block';
+    document.getElementById('newrecipe').style.display = 'none';
+    document.getElementById('myrecipe').style.display = 'none';
+   
   }
-  const handleMyrecipes =() =>{
-    document.getElementsByClassName('results').style.visibility = 'hidden';
-    console.log("thisbutton")
+  const handleMyrecipes =() =>{ 
+    document.getElementById('myrecipe').style.display = 'inline-block';
+    document.getElementById('newrecipe').style.display = 'none';
+    document.getElementById('explore').style.display = 'none';
+    retrieveuserrecipe()
+
   }
   const handleAddrecipe =() =>{
-    console.log('hi2')
-    SetAddrecipe(!Addrecipe)
+    document.getElementById('newrecipe').style.display = 'block';
+    document.getElementById('explore').style.display = 'none';
+    document.getElementById('myrecipe').style.display = 'none';
+   
+    
   }
   const handleTop =() =>{
     console.log('This button is under construction')
@@ -40,13 +52,21 @@ function Eggsbee() {
     setSelectedArray(recipe)
     
   }
+
+  const retrieveuserrecipe= async() =>{
+          const response = await fetch(BASE_URL+'recipes/owneremail?email='+user.email)
+          const recipes = await response.json()
+          SetMine(recipes)
+          console.log(recipes)
+  }
+  const triggerdlt= async(recipe) =>{
+    axios.delete(BASE_URL+'recipes/id?id='+ `${recipe._id}`)
+  }
  
   useEffect(() => {
     
     const callmodel=() =>{
-      
       SetCardbutton(!cardbutton)
-      
     }
     callmodel();
      // This is be executed when `loading` state changes
@@ -93,10 +113,11 @@ function Eggsbee() {
           }
         }
         SetRecipe(recipes)
-        console.log(recipes[0].Premium)
+        
       }
       fetchData()
     },[])
+ 
   
   return (
     <div>
@@ -113,8 +134,8 @@ function Eggsbee() {
     <button onClick={handleTop}>Top Recipes</button>
 
     </div>
-      {Addrecipe ?(
-      <div className="results"> 
+      
+      <div className="results" id="newrecipe"> 
       
       <h2>Add New Recipe</h2>
       <input type="text" placeholder='Title' value={Title} onChange={(e) => SetTitle(e.target.value)}></input>
@@ -130,9 +151,9 @@ function Eggsbee() {
                 <h3>Your recipe has been added. </h3>):("")} 
                
               
-      </div> ):(
+      </div> 
         
-        <div className='results'>
+        <div className='results' id="explore">
           <Container>
             <Row>
             {Recipes.map((Recipe) => (
@@ -160,7 +181,7 @@ function Eggsbee() {
                                 </Modal.Header>
 
                                 <Modal.Body><b>Description: </b>{selectedArray.Description}<br></br>
-                                            <b>Materials:</b> <ul>{selectedArray.Materials.map((ingredient)=>(
+                                                 <b>Materials:</b> <ul>{selectedArray.Materials.map((ingredient)=>(
                                             <li>{ingredient}</li>
                                             ))}</ul>
                                             <b>Instructions:</b><ol>{selectedArray.Instruction.map((instruction)=>(
@@ -189,10 +210,63 @@ function Eggsbee() {
           </Container>
 
 
-         
+          
         </div>
-       
-      )}
+       <div className='results' id="myrecipe">
+       <Row>
+       {mine.map((Recipe) => (
+              
+              <Col xs={12} sm={6} md={4}>
+                
+                  <Card key ={Recipe._id} className='cards'>
+                      
+                      <Card.Img className="cardimg" src={Recipe.Img[0] } />
+
+                      <Card.Body>
+                          <Card.Title><b>{Recipe.Title}</b></Card.Title>
+                          <Card.Text>{Recipe.Description.split(".",1)}</Card.Text>
+                          <Button ><FaEdit/> </Button>
+                          <Button onClick={()=>triggerdlt(Recipe)}><FaTrash/> </Button>
+                          <div className='cardfooter' id={ Recipe.Premium === true ? 'prem' : 'norm'}>Premium</div>
+                      </Card.Body>
+                      {cardbutton ?(
+                      <Modal show={mycardbutton} onHide={()=>SetMycardbutton(false)}>
+
+                          <Modal.Header closeButton>
+
+                              <Modal.Title><b>{selectedArray.Title}</b></Modal.Title>
+
+                          </Modal.Header>
+
+                          <Modal.Body><b>Description: </b>{selectedArray.Description}<br></br>
+                                      <b>Materials:</b> <ul>{selectedArray.Materials.map((ingredient)=>(
+                                      <li>{ingredient}</li>
+                                      ))}</ul>
+                                      <b>Instructions:</b><ol>{selectedArray.Instruction.map((instruction)=>(
+                                      <li>{instruction}</li>
+                                      ))}</ol>
+                          </Modal.Body>
+
+                          <Modal.Footer>
+
+                             <Button onClick={()=>SetMycardbutton(false)}>
+
+                              Close
+
+                            </Button>
+
+                          </Modal.Footer>
+
+                      </Modal>):("")}
+                        </Card>
+                 
+                  </Col>
+                  
+         
+          ))}    
+          </Row>                       
+       </div>
+      
     </div>
     </div>
   )
